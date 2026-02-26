eb7fa8947d051a6c4946979994128f652e8794a0
Thomas Weissel <valueerror@gmail.com>
2026-02-26 17:44:29 +0100

    introduces !

--- student/packages/renderer/src/pages/student.vue
+++ student/packages/renderer/src/pages/student.vue
@@ -104,7 +104,7 @@
                     <div style="display:flex; flex-direction: row; justify-content: space-between; padding:0px;">
                         <div style="width:130px; display:inline-block; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;"> {{server.servername}}</div>  
                         
-                        <div v-if="server.version == version" class="badge btn-warning" style="width:170px; height:20px; vertical-align: text-bottom; margin-top: 2px; display: inline;" :title="$t('student.outdatedinfo')"> {{$t('student.outdated')}} {{ server.version }} </div>
+                        <div v-if="server.version !== version" class="badge btn-warning" style="width:170px; height:20px; vertical-align: text-bottom; margin-top: 2px; display: inline;" :title="$t('student.outdatedinfo')"> {{$t('student.outdated')}} {{ server.version }} </div>
                         <div v-else-if="server.bip" class="badge btn-teal" style="width:70px; height:20px; vertical-align: text-bottom; margin-top: 2px; display: inline;"> BiP Exam</div>
                         <div v-else  style="width:70px; height:20px; vertical-align: text-bottom; margin-top: 2px; display: inline;"> </div>
                     </div>
