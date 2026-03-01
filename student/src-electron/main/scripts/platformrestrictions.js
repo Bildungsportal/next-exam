aa22fa7b4ceeee1d83170be9c1ded92b6ab6a452
Thomas Weissel <valueerror@gmail.com>
2026-03-01 21:14:10 +0100

    whatsapp

--- student/src-electron/main/scripts/platformrestrictions.js
+++ student/src-electron/main/scripts/platformrestrictions.js
@@ -52,7 +52,7 @@ let configStore = {
 };
 
 // list of apps we do not want to run in background
-const appsToClose = ['Google Chrome', 'chrome', 'google-chrome', 'Microsoft Edge', 'msedge', 'firefox', 'safari', 'brave', 'opera', 'chatgpt', 'ChatGPT', 'NortonSecurity', 'NAV', 'Teams', 'ms-teams', 'zoom.us', 'Microsoft Teams', 'discord', 'zoom', 'teams', 'teamviewer', 'skypeforlinux', 'skype', 'anydesk'];
+const appsToClose = ['whatsapp','Google Chrome', 'chrome', 'google-chrome', 'Microsoft Edge', 'msedge', 'firefox', 'safari', 'brave', 'opera', 'chatgpt', 'ChatGPT', 'NortonSecurity', 'NAV', 'Teams', 'ms-teams', 'zoom.us', 'Microsoft Teams', 'discord', 'zoom', 'teams', 'teamviewer', 'skypeforlinux', 'skype', 'anydesk'];
 
 async function enableRestrictions(winhandler) {
     if (config.development) { return; }
