6d6451e227bb614196f86fd7e5ba0f49f66ad361
Thomas Weissel <valueerror@gmail.com>
2026-04-09 08:57:59 +0200

    add display server to log

--- student/src-electron/electron-main.js
+++ student/src-electron/electron-main.js
@@ -99,6 +99,7 @@ log.debug(`main: V8 version: ${process.versions.v8}`)
 log.debug(`main: OS: ${process.platform} ${process.arch}`)
 log.debug(`main: Arch: ${process.arch}`)
 log.debug(`main: Desktop: ${platformDispatcher.desktopName}`)
+log.debug(`main: Display server: ${platformDispatcher.displayServer}`)
 
 
 WindowHandler.init(multicastClient, config)  // mainwindow, examwindow, blockwindow
