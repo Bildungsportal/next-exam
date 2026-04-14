4296c770da456f1190e7eb33c00f382cee959b00
Thomas Weissel <valueerror@gmail.com>
2026-04-14 21:18:29 +0200

    config

--- student/src-electron/main/config.js
+++ student/src-electron/main/config.js
@@ -27,7 +27,7 @@ const config = {
     isPuavo: false,
     
     version: '2.0.0.1',
-    buildDate: '20260410',
+    buildDate: '20260413',
     buildNumber: '1',
     info: 'Release'
 }
--- teacher/src-electron/main/config.js
+++ teacher/src-electron/main/config.js
@@ -39,7 +39,7 @@ const config = {
     },
 
     version: '2.0.0.1',
-    buildDate: '20260410',
+    buildDate: '20260414',
     buildNumber: '1',
     info: 'Release'
 }
