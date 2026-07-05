89c83234bcc657369573bf97840fe6eeaf1ea105
Thomas Weissel <valueerror@gmail.com>
2026-07-05 13:20:01 +0200

    fix: ask before replace in editor even if sections are active, replace silent only on section switch

--- student/src/pages/editor.vue
+++ student/src/pages/editor.vue
@@ -2615,7 +2615,7 @@ export default {
         });
         signalBridge.on('backup', (event, filename) => {
             console.log("editor @ backup: Replace event received ")
-            this.loadBackupFileSilent(filename)
+            this.loadHTML(filename)
         });
         signalBridge.on('loadfilelist', () => {
             //console.log("editor @ loadfilelist: Reload Files event received ")
